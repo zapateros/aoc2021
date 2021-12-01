b311890537af1162ccefa9bf885e40d8c81eee1c
Paul <plschoenmakers@hotmail.com>
2021-12-01 20:05:54 +0100

    Update day_01.R

--- solutions/day_01.R
+++ solutions/day_01.R
@@ -5,5 +5,5 @@ cat("Day 1 Star 1: ", output)
 
 # Star 2
 # (x2 + x3 + x4) - (x1 + x2 + x3) = x4 - x1
-output <- sum((input[-c(1:3)] > head(input, -3)))
+output <- sum(input[-c(1:3)] > head(input, -3))
 cat("Day 1 Star 2: ", output)
