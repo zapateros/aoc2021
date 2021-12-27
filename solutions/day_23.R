b969ae1299cb88d89ea7a56321f731b8167510d2
Paul <plschoenmakers@hotmail.com>
2021-12-27 17:40:51 +0100

    Create day_23.R

---
+++ solutions/day_23.R
@@ -0,0 +1,4 @@
+# I did both problems manually. Both problems took me like 10 minutes (not counting in breakfast and coffeetime). The first one on my phone, the second in excel
+# The second problem looked a lot harder as there could be quite a lot more steps. However, when taking into account that you 
+# want to move the D's the least, then the C's etc. It was not very hard to solve. I'm not sure if I was just lucky with the orders
+# Especially because the number 1 solved it in 23 minutes. Well, I cant prove my solving times right..?
